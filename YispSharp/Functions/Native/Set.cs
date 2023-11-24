2f3b665b301c1d467fad994182bf5fa1eb0afca5
klnesmith <klnesmith@crimson.ua.edu>
2023-11-24 14:10:42 -0600

    Forbid set and define within nested code.

--- YispSharp/Functions/Native/Set.cs
+++ YispSharp/Functions/Native/Set.cs
@@ -1,4 +1,5 @@
-﻿using YispSharp.Data;
+﻿using System.Diagnostics;
+using YispSharp.Data;
 using YispSharp.Exceptions;
 using YispSharp.Utils;
 
@@ -13,6 +14,12 @@ namespace YispSharp.Functions.Native
 
         public object Call(Interpreter interpreter, List<SExpr> arguments)
         {
+            // Stop sets in nested code
+            if (!new StackFrame(4).GetMethod().Name.Equals("Interpret"))
+            {
+                throw new RuntimeException("Cannot set an object in nested code.");
+            }
+
             if (arguments[0] is SExpr.Atom a && a.Value is Token t && t.Type == TokenType.Symbol)
             {
                 object value = interpreter.Evaluate(arguments[1]);
